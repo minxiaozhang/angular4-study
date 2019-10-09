f9e8f2f905199f57a78914ddcf958b7117701b78
张敏 <zhangmin003@sinosoft.com.cn>
2019-10-09 15:28:26 +0800

    多环境配置

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  server: '开发环境'
+};
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+console.log("当前服务环境端口是:",environment.server)
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
